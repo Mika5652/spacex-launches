02d25501f0d86fe396b2ec70f772aa59f6d4b618
Michael Pohl <15653162+Mika5652@users.noreply.github.com>
2023-06-24 19:50:07 +0200

    Remove smallImageUrl from PastLaunch model

--- SpaceXLaunches/SpaceXLaunches/Features/PastLaunchesFeature/Sources/Model/PastLaunch.swift
+++ SpaceXLaunches/SpaceXLaunches/Features/PastLaunchesFeature/Sources/Model/PastLaunch.swift
@@ -1,35 +1,22 @@
 import Foundation
 
-struct PastLaunch: Identifiable, Decodable {
+struct PastLaunch: Identifiable, Decodable, Hashable {
     let id: String
     let dateUtc: Date
     let name: String
-    let smallImageUrl: String
 
     private enum RootCodingKeys: String, CodingKey {
         case id
         case dateUtc = "date_utc"
         case name
-        case links
-    }
-
-    private enum LinksCodingKeys: String, CodingKey {
-        case patch
-    }
-
-    private enum PatchCodingKeys: String, CodingKey {
-        case small
     }
 
     init(from decoder: Decoder) throws {
         let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
-        let linksContainer = try rootContainer.nestedContainer(keyedBy: LinksCodingKeys.self, forKey: .links)
-        let patchContainer = try linksContainer.nestedContainer(keyedBy: PatchCodingKeys.self, forKey: .patch)
 
         id = try rootContainer.decode(String.self, forKey: .id)
         dateUtc = try rootContainer.decode(String.self, forKey: .dateUtc).toDate
         name = try rootContainer.decode(String.self, forKey: .name)
-        smallImageUrl = try patchContainer.decode(String.self, forKey: .small)
     }
 }
 
