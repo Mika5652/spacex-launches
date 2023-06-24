43f8e9c7bf78bef13701e750fde28b24d612b22a
Michael Pohl <15653162+Mika5652@users.noreply.github.com>
2023-06-24 10:00:40 +0200

    Add Kits and APIClient dependency to Features

--- SpaceXLaunches/SpaceXLaunches/Features/Package.swift
+++ SpaceXLaunches/SpaceXLaunches/Features/Package.swift
@@ -4,12 +4,21 @@ import PackageDescription
 
 let package = Package(
     name: "Features",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         .library(name: "PastLaunchesFeature", targets: ["PastLaunchesFeature"]),
+    ],
+    dependencies: [
+        .package(path: "../Kits")
     ],
     targets: [
         .target(
             name: "PastLaunchesFeature",
+            dependencies: [
+                .product(name: "APIClient", package: "Kits")
+            ],
             path: "PastLaunchesFeature/Sources"
         )
     ]
