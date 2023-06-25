90307899c62313653e713f531b5a9bf1691f8ac4
Michael Pohl <15653162+Mika5652@users.noreply.github.com>
2023-06-25 09:57:37 +0200

    Increase deployment target to iOS 16

--- SpaceXLaunches/SpaceXLaunches/Kits/Package.swift
+++ SpaceXLaunches/SpaceXLaunches/Kits/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Kits",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v16)
     ],
     products: [
         .library(name: "APIClient", targets: ["APIClient"])
